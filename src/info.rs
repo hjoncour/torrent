c80c2a0b6cfada20e0d108fef30ebdd779c73765
SanteauX <hugo.joncour.k2o@gmail.com>
2023-09-12 22:47:02 -0400

    fix

--- src/info.rs
+++ src/info.rs
@@ -1,8 +1,10 @@
+use std::fs::File;
+use std::io::Write;
+
 use bendy::decoding::{Error, FromBencode, Object, ResultExt};
 use bendy::encoding::AsString;
 use serde::{Deserialize, Serialize};
 
-// File related information (Single-file format)
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Info {
     pub piece_length: String,
@@ -29,37 +31,33 @@ impl FromBencode for Info {
         let mut dict_dec: bendy::decoding::DictDecoder<'_, '_> = object.try_into_dictionary()?;
         while let Some(pair) = dict_dec.next_pair()? {
             match pair {
-                (b"length", value)          => file_length  = value.try_into_integer().context("file.length").map(ToString::to_string).map(Some)?,
-                (b"name", value)            => name         = String::decode_bencode_object(value).context("name").map(Some)?,
-                (b"piece length", value)    => piece_length = value.try_into_integer().context("length").map(ToString::to_string).map(Some)?,
-                (b"pieces", value)          => pieces       = AsString::decode_bencode_object(value).context("pieces").map(|bytes| Some(bytes.0))?,
-                // (b"files", value)           => files        = Vec::decode_bencode_object(value).context("files").map(Some)?,
-
-                (unknown_field, value) => {
+                (b"length", value)              => file_length  = value.try_into_integer().context("file.length").map(ToString::to_string).map(Some)?,
+                (b"name", value)                => name         = String::decode_bencode_object(value).context("name").map(Some)?,
+                (b"piece length", value)        => piece_length = value.try_into_integer().context("length").map(ToString::to_string).map(Some)?,
+                (b"pieces", value)              => pieces       = AsString::decode_bencode_object(value).context("pieces").map(|bytes| Some(bytes.0))?,
+                //(b"files", value)               => files        = Vec::decode_bencode_object(value).context("files").map(Some)?,
+                
+                (unknown_field, value)   => {
                     let field_name: String = String::from_utf8_lossy(unknown_field).to_string();
                     let value_as_string: String;
-
-                    match &value {
+                    println!("FIELD NAME: {}", &field_name);
+                    match value {
                         Object::Bytes(bytes) => {
-                            value_as_string = String::decode_bencode_object(bendy::decoding::Object::Bytes(bytes)).unwrap_or_else(|_| "Unknown".to_string());
+                            value_as_string = String::decode_bencode_object(bendy::decoding::Object::Bytes(bytes)).unwrap_or_else(|_| "Unknown Bytes".to_string());
                         }
                         Object::Integer(integer) => {
-                            value_as_string = u64::decode_bencode_object(bendy::decoding::Object::Integer(integer)).map(|u| u.to_string()).unwrap_or_else(|_| "Unknown".to_string());
+                            value_as_string = u64::decode_bencode_object(bendy::decoding::Object::Integer(integer)).map(|u| u.to_string()).unwrap_or_else(|_| "Unknown Integer".to_string());
                         }
                         Object::List(list) => {
-                            // Not working
-                            // value_as_string = Vec::<String>::decode_bencode_object(Object::List(list)).map(|v: Vec<String>| v.join(", ")).unwrap_or_else(|_| "Unknown".to_string());
-                            println!("missing list: __");//                            println!("list: {:#?}", list);
-                            value_as_string = "".to_owned();
+                            value_as_string = Vec::<String>::decode_bencode_object(Object::List(list)).map(|v: Vec<String>| v.join(", ")).unwrap_or_else(|_| "Unknown List".to_string());
                         }
-                        Object::Dict(_) => {
-                            // TO FIX
-                            println!("missing dict: __");//                            println!("list: {:#?}", list);
+                        Object::Dict(dict) => {
+                            println!("dict: ");
                             value_as_string = "Dictionary".to_string();
+                            // Unsure if it is working
                         }
                     }
                     
-               
                     let tuple: (String, String) = (field_name, value_as_string);
                     if let Some(ref mut fields) = other_fields {
                         fields.push(tuple);
@@ -73,7 +71,6 @@ impl FromBencode for Info {
         let piece_length:   String      = piece_length.ok_or_else(|| Error::missing_field("piece_length"))?;
         let pieces:         Vec<u8>     = pieces.ok_or_else(|| Error::missing_field("pieces"))?;
 
-        // Check that we discovered all necessary fields
         Ok(Info {file_length, name, piece_length, pieces, files})
     }
 }
--- src/meta_info.rs
+++ src/meta_info.rs
@@ -5,16 +5,16 @@ use crate::info::Info;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct MetaInfo {
-    pub announce: Option<String>,                               // Required fields for all torrents
-    pub info: Info,                                     // Required fields for all torrents
+    pub announce: Option<String>,                                // Required fields for all torrents
+    pub info: Info,                                              // Required fields for all torrents
 
-    pub announce_list:          Option<Vec<String>>,    // Optional
-    pub comment:                Option<String>,         // Optional
-    pub created_by:             Option<String>,         // Optional
-    pub creation_date:          Option<u64>,            // Optional
-    pub encoding:               Option<String>,         // Optional
-    pub http_seeds:             Option<Vec<String>>,    // Optional
-    pub other_fields:           Option<Vec<(String, String)>>,    // Optional
+    pub announce_list:          Option<Vec<String>>,             // Optional
+    pub comment:                Option<String>,                  // Optional
+    pub created_by:             Option<String>,                  // Optional
+    pub creation_date:          Option<u64>,                     // Optional
+    pub encoding:               Option<String>,                  // Optional
+    pub http_seeds:             Option<Vec<String>>,             // Optional
+    pub other_fields:           Option<Vec<(String, String)>>,   // Optional
 }
 
 impl FromBencode for MetaInfo {
@@ -25,16 +25,16 @@ impl FromBencode for MetaInfo {
     where
         Self: Sized,
     {
-        let mut announce:       Option<String>                = None;     // Required fields for all torrents
-        let mut info:           Option<Info>                  = None;     // Required fields for all torrents
+        let mut announce:       Option<String>                = None;               // Required fields for all torrents
+        let mut info:           Option<Info>                  = None;               // Required fields for all torrents
 
-        let mut announce_list:  Option<Vec<String>>           = None;     // Optional Field
-        let mut comment:        Option<String>                = None;     // Optional Field
-        let mut created_by:     Option<String>                = None;     // Optional Field
-        let mut creation_date:  Option<u64>                   = None;     // Optional Field
-        let mut encoding:       Option<String>                = None;     // Optional Field
-        let mut http_seeds:     Option<Vec<String>>           = None;     // Optional Field
-        let mut other_fields:   Option<Vec<(String, String)>> = Some(Vec::new()); // Initialize other_fields as Some(Vec::new())
+        let mut announce_list:  Option<Vec<String>>           = None;               // Optional Field
+        let mut comment:        Option<String>                = None;               // Optional Field
+        let mut created_by:     Option<String>                = None;               // Optional Field
+        let mut creation_date:  Option<u64>                   = None;               // Optional Field
+        let mut encoding:       Option<String>                = None;               // Optional Field
+        let mut http_seeds:     Option<Vec<String>>           = None;               // Optional Field
+        let mut other_fields:   Option<Vec<(String, String)>> = Some(Vec::new());   // Initialize other_fields as Some(Vec::new())
 
         let mut dict_dec: bendy::decoding::DictDecoder<'_, '_> = object.try_into_dictionary()?;
 
@@ -43,7 +43,9 @@ impl FromBencode for MetaInfo {
                 (b"announce", value)        => announce         = String::decode_bencode_object(value).context("announce").map(Some)?,
                 (b"announce_list", value)   => announce_list    = Vec::decode_bencode_object(value).context("announce_list").map(Some)?,
                 (b"comment", value)         => comment          = String::decode_bencode_object(value).context("comment").map(Some)?,
+                (b"created by", value)      => created_by       = String::decode_bencode_object(value).context("created by").map(Some)?,
                 (b"creation date", value)   => creation_date    = u64::decode_bencode_object(value).context("creation_date").map(Some)?,
+                (b"encoding", value)        => encoding         = String::decode_bencode_object(value).context("encoding").map(Some)?,
                 (b"http seeds", value)      => http_seeds       = Vec::decode_bencode_object(value).context("http_seeds").map(Some)?,
                 (b"info", value)            => info             = Info::decode_bencode_object(value).context("info").map(Some)?,
 
@@ -51,38 +53,31 @@ impl FromBencode for MetaInfo {
                     let field_name: String = String::from_utf8_lossy(unknown_field).to_string();
                     let value_as_string: String;
 
-                    match &value {
+                    match value {
                         Object::Bytes(bytes) => {
-                            value_as_string = String::decode_bencode_object(bendy::decoding::Object::Bytes(bytes)).unwrap_or_else(|_| "Unknown".to_string());
+                            value_as_string = String::decode_bencode_object(bendy::decoding::Object::Bytes(bytes)).unwrap_or_else(|_| "Unknown Bytes".to_string());
                         }
                         Object::Integer(integer) => {
-                            value_as_string = u64::decode_bencode_object(bendy::decoding::Object::Integer(integer)).map(|u| u.to_string()).unwrap_or_else(|_| "Unknown".to_string());
+                            value_as_string = u64::decode_bencode_object(bendy::decoding::Object::Integer(integer)).map(|u| u.to_string()).unwrap_or_else(|_| "Unknown Integer".to_string());
                         }
                         Object::List(list) => {
-                            // Not working
-                            // value_as_string = Vec::<String>::decode_bencode_object(Object::List(list)).map(|v: Vec<String>| v.join(", ")).unwrap_or_else(|_| "Unknown".to_string());
-                            println!("list: {:#?}", list);
-                            value_as_string = "".to_owned();
+                            value_as_string = Vec::<String>::decode_bencode_object(Object::List(list)).map(|v: Vec<String>| v.join(", ")).unwrap_or_else(|_| "Unknown List".to_string());
                         }
                         Object::Dict(_) => {
                             // Handle the case where the value is a dictionary if needed
                             value_as_string = "Dictionary".to_string();
                         }
                     }
-                    
                
                     let tuple: (String, String) = (field_name, value_as_string);
                     if let Some(ref mut fields) = other_fields {
                         fields.push(tuple);
                     }
                 }
-                
-
             }
         }
-
+        
         let info: Info = info.ok_or_else(|| Error::missing_field("info"))?;
-
         Ok(MetaInfo {
             announce,
             info,
